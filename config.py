31152a37197dfd2c250b0e8e0ffa5c13e8984e76
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-07-10 23:29:13 +0300

    Update and rename exampleconfig.py to config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+from sample_config import Config
+class Development(Config):
+    APP_ID = 9365736
+    API_HASH = "3fa1b478ca72a73d267e10188556cc34"
+    ALIVE_NAME = "هادي"
+    STRING_SESSION = "1BJWap1wBu3AnKuJGX5y4xXdDiRBeQ-kN9jLxw-i-7LWnUTF1a5OeV3px8lXKnVC5OWsIwdyymr5_944SAgCrTWKxVr7CHVeHZrLv04V4IE8C73yma3VMAdeaHpaZnFDiktg2kmY0KaHq9KAhIGdRWyvtwDtZ8pmCUY5uulHbpb7iuS5zVsy0_DlyqnFho984V0JrKn6-oRuVjfwP33alvSRaEyFenFf-wQE6eb1opYpRImf1bDAIJLNomHy0NECjeiaHljithlZm-1Qw8TeskITBdqjdST1pjby0APxMYxDZd3m29xrQxDjWauayARW9ph1PrCbH9noWQpCb1aSFZE4Ss3ET7a4="
+    TG_BOT_TOKEN = "5111897157:AAGKx8QRGetJKugii06AwtLoHqE51AyYMbs"
+    TG_BOT_USERNAME = "Q71bot"
+    DB_URI = "postgres://hbifpcsx:H1KGLnvFlLPFs5u9g6tjhNb9bbdA8zmc@salt.db.elephantsql.com/hbifpcsx"
+    PRIVATE_GROUP_BOT_API_ID = -1001659591037
+    COMMAND_HAND_LER = "."
+    SUDO_COMMAND_HAND_LER = "."
+    TZ = "Asia/Baghdad"
+
+# تليثون العرب - @IQTHON 
