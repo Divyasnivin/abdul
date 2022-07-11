254fcab3f76c034a77339aa620515e2ddb4afd7a
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-07-11 16:04:51 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,7 +3,7 @@ class Development(Config):
     APP_ID = 9365736
     API_HASH = "3fa1b478ca72a73d267e10188556cc34"
     ALIVE_NAME = "هادي"
-    STRING_SESSION = "1BJWap1wBu3AnKuJGX5y4xXdDiRBeQ-kN9jLxw-i-7LWnUTF1a5OeV3px8lXKnVC5OWsIwdyymr5_944SAgCrTWKxVr7CHVeHZrLv04V4IE8C73yma3VMAdeaHpaZnFDiktg2kmY0KaHq9KAhIGdRWyvtwDtZ8pmCUY5uulHbpb7iuS5zVsy0_DlyqnFho984V0JrKn6-oRuVjfwP33alvSRaEyFenFf-wQE6eb1opYpRImf1bDAIJLNomHy0NECjeiaHljithlZm-1Qw8TeskITBdqjdST1pjby0APxMYxDZd3m29xrQxDjWauayARW9ph1PrCbH9noWQpCb1aSFZE4Ss3ET7a4="
+    STRING_SESSION = "1BJWap1wBu8MYaAS7Xcbnl4M-qym80Q7WcoTR0c38A_Hf3S3t2qL1dE1vPeT2_FGmwC9n9DV1rQzhdTiQdH-5u4BiCEfbAtUacCwThZK_MK6I0Dib1-fZMpRKK-zChLp9YqRUvu_A2hYGShzq2pgr517mFjqmiqzoZ2R0AferTduQaWrRGPSLf0zFUF0W14HjqRu9IZfQTJcM82mZ7yjEiLqhHRcGF_m-uxcB3Xr-EzNRUOhmkqSdCc2aQC-3KzxMXzZQzlrT_Gpomv8e00xGAcbiyVx1OTtC09mOjOSlBvWPVfnYoHi8D-9ZcJvNyg_1x5QEyAEUNuHJQuhh4jUwsDmQL1JIXrc="
     TG_BOT_TOKEN = "5111897157:AAGKx8QRGetJKugii06AwtLoHqE51AyYMbs"
     TG_BOT_USERNAME = "Q71bot"
     DB_URI = "postgres://hbifpcsx:H1KGLnvFlLPFs5u9g6tjhNb9bbdA8zmc@salt.db.elephantsql.com/hbifpcsx"
