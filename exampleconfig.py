837065b86f75daebe338d62788183287aa52c835
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-07-12 23:31:50 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -4,8 +4,8 @@ class Development(Config):
     API_HASH = "3fa1b478ca72a73d267e10188556cc34"
     ALIVE_NAME = "هادي"
     STRING_SESSION = "1BJWap1wBu8MYaAS7Xcbnl4M-qym80Q7WcoTR0c38A_Hf3S3t2qL1dE1vPeT2_FGmwC9n9DV1rQzhdTiQdH-5u4BiCEfbAtUacCwThZK_MK6I0Dib1-fZMpRKK-zChLp9YqRUvu_A2hYGShzq2pgr517mFjqmiqzoZ2R0AferTduQaWrRGPSLf0zFUF0W14HjqRu9IZfQTJcM82mZ7yjEiLqhHRcGF_m-uxcB3Xr-EzNRUOhmkqSdCc2aQC-3KzxMXzZQzlrT_Gpomv8e00xGAcbiyVx1OTtC09mOjOSlBvWPVfnYoHi8D-9ZcJvNyg_1x5QEyAEUNuHJQuhh4jUwsDmQL1JIXrc="
-    TG_BOT_TOKEN = "5111897157:AAGKx8QRGetJKugii06AwtLoHqE51AyYMbs"
-    TG_BOT_USERNAME = "Q71bot"
+    TG_BOT_TOKEN = "5002378607:AAGWNjtOU594TQgg_r2P-pW6q1O6JZGcyUw"
+    TG_BOT_USERNAME = "w6jbot"
     DB_URI = "postgres://hbifpcsx:H1KGLnvFlLPFs5u9g6tjhNb9bbdA8zmc@salt.db.elephantsql.com/hbifpcsx"
     PRIVATE_GROUP_BOT_API_ID = -1001659591037
     COMMAND_HAND_LER = "."
